1a4145f3c9e846f72959e7f53930146e253eb5af
Caio Quinta <caio@contratado.me>
2014-11-04 09:28:31 -0500

    Projeto One Month Rails
    Projeto em rails do curso one month rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CaioApp::Application.config.secret_key_base = '475f1f9001fa6084ba92eafd78389fb11ed9f98871aec99abf716ef72ac015d160e2b08a446b126e72b6ff92471c7922848c132d7eb1215c0019a35ca64baabd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CaioApp::Application.config.session_store :cookie_store, key: '_caio_app_session'
